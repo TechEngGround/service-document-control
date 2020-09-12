998c39b655bc18c2b9602a91e8b32cd700ba6628
Paulo Leal <leal.ph@outlook.com>
2020-09-12 15:47:12 -0300

    Correção na build

--- docker/prod.Dockerfile
+++ docker/prod.Dockerfile
@@ -3,12 +3,11 @@ FROM node:12 AS builder
 COPY . /app/
 WORKDIR /app/
 
-RUN cd ../types && yarn install && yarn build && cd ../service-document-control && yarn && yarn pkg 
+RUN cd types && yarn install && yarn build && cd ../service-document-control && yarn && yarn pkg 
 
-FROM node:12
-
-USER node
+FROM debian:10.5-slim
+ENV NODE_ENV=production
 WORKDIR /app/
-COPY --from=builder /app/document.app /app/document.app
+COPY --from=builder /app/service-document-control/document.app /app/document.app
 
-CMD ["./document.app"]
+CMD /app/document.app
--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,13 @@ require('dotenv').config();
 import App from './app'
 import Logger from './util/log'
 
-const port = process.env.SERVER_PORT && parseInt(process.env.SERVER_PORT) || 3002;
+const port = (process.env.SERVER_PORT && parseInt(process.env.SERVER_PORT)) || 3002;
 const app = new App();
 
 const start = async () => {
   try {
     Logger.info('Starting http server...');
-    app.server.listen(port, '0.0.0.0', (error: any, address: string) => {
-      if (error) {
-        Logger.error(`Server not initialized, error: ${error}`);
-        process.exit(1);
-      }
+    app.server.listen(port, '0.0.0.0', () => {
       Logger.info(`Server started.`);
     });
   } catch (error) {
